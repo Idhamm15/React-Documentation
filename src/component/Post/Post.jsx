d3da5d92bef3f1a214a186e848bb58ca3a3918e0
Idhamm15 <idhamtamvanz123@gmail.com>
2023-12-15 15:06:45 +0700

    Pemanggilan API (DELETE API)

--- src/component/Post/Post.jsx
+++ src/component/Post/Post.jsx
@@ -14,8 +14,9 @@ const Post = (props) => {
                     <img style={fixImage} src={Ayam} alt="" />
                 </div>
                 <div className="content">
-                    <p className="title">{props.title}</p>
-                    <p className="desc">{props.desc}</p>
+                    <p className="title">{props.data.title}</p>
+                    <p className="desc">{props.data.body}</p>
+                    <button className='remove' onClick={()=> props.remove(props.data.id)}>remove</button>
                 </div>
         </div>
         </Fragment>
--- src/container/BlogPost/BlogPost.jsx
+++ src/container/BlogPost/BlogPost.jsx
@@ -10,25 +10,25 @@ class BlogPost extends Component {
         post : []
     }
 
-    // Get API menggunakan fetching data
-    // componentDidMount() {
-    //     fetch('https://jsonplaceholder.typicode.com/posts')
-    //     .then(response => response.json())
-    //     .then(json => {
-    //         this.setState({
-    //             post : json
-    //         })
-    //     })
-    // }
-
-    // Get API menggunakan Axios data
-    componentDidMount() {
+    getPostAPI=()=>{
         axios.get('http://localhost:3000/posts')
         .then((result) => {
             this.setState({
                 post: result.data
             })
         })
+    }
+
+    // handle Remove
+    handleRemove = (data) => {
+        axios.delete(`http://localhost:3000/posts/${data}`).then((res)=>{
+            this.getPostAPI()
+        })
+    }
+
+    // Get API menggunakan Axios data
+    componentDidMount() {
+        this.getPostAPI();
     }
 
 
@@ -38,7 +38,7 @@ class BlogPost extends Component {
             <p className="section-title">Blog Post</p>
             {
                 this.state.post.map(post => {
-                    return <Post key={post.id} title={post.title} desc={post.body} />
+                    return <Post key={post.id} data={post} remove={this.handleRemove}  />
                 })
             }  
             <Post/>  
